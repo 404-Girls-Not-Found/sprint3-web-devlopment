e93f63954fdc27933eb47d9c87859cde6970ef6a
Estela <estela.mariano@icloud.com>
2026-08-25 17:50:16 -0300

    feat: entrada Modo Aula

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,52 @@ import { useState } from "react";
 
 import EscolherPrototipo from "./components/escolherPrototipo/EscolherPrototipo";
 import CameraPrototipo from "./components/camera/CameraPrototipo";
+import EntradaModoAula from "./components/camera/1entradaModoAula/EntradaModoAula";
 
 function App() {
   const [telaAtual, setTelaAtual] = useState("escolha");
 
+  const [entradaModoAulaAberta, setEntradaModoAulaAberta] = useState(false);
+
   function abrirCamera() {
-    setTelaAtual("escolha");
+    setTelaAtual("camera");
   }
 
   function voltarParaEscolhaPrototipo() {
+    setEntradaModoAulaAberta(false);
     setTelaAtual("escolha");
+  }
+
+  function abrirEntradaModoAula() {
+    setEntradaModoAulaAberta(true);
+  }
+
+  function fecharEntradaModoAula() {
+    setEntradaModoAulaAberta(false);
+  }
+
+  function configurarModoAula () {
+    setEntradaModoAulaAberta(false);
   }
 
   if (telaAtual === "camera") {
     return (
-      <CameraPrototipo onVoltar={voltarParaEscolhaPrototipo} />
+      <CameraPrototipo
+      onVoltar={voltarParaEscolhaPrototipo}
+      onAbrirModoAula={abrirEntradaModoAula}
+      >
+
+        <EntradaModoAula
+          aberto={entradaModoAulaAberta}
+          onFechar={fecharEntradaModoAula}
+          onConfigurar={configurarModoAula}
+        />
+      </CameraPrototipo>
     );
   }
 
   return (
-    <EscolherPrototipo onAbrirCamera={() => setTelaAtual("camera")}/>
+    <EscolherPrototipo onAbrirCamera={abrirCamera}/>
   );
 }
 
